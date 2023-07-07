6d386caedb008801eedf4e27ab49423211aa027d
Moien <137896500+moienheydari@users.noreply.github.com>
2023-07-07 16:54:22 +0330

    App build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      Learn to code by watching others
+
+      See how experienced developers solve problems in real-time. Watching scripted tutorials is great,
+      but understanding how developers think is invaluable.
+
+      Try it free 7 days then $20/mo. thereafter
+
+      First Name
+      Last Name
+      Email Address
+      Password
+
+      Claim your free trial
+
+      By clicking the button, you are agreeing to our Terms and Services
+
+      <footer>
+        <p class="attribution">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+          Coded by <a href="#">Your Name Here</a>.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </footer>
     </div>
   );
 }
